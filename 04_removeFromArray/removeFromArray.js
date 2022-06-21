c113a60e887b4e4217bc504e4fc47b6aaffa95e5
Josh Crisostomo <joshuacr22@gmail.com>
2022-06-20 22:45:13 -0400

    complete 05_sumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,8 +3,5 @@ const removeFromArray = function(arr, ...removeFromArray) {
     return arr.filter(item => !removeFromArray.includes(item));
 };
 
-let sampleArr = [0,1,2,3,4,5];
-console.log(removeFromArray(sampleArr, 3,2));
-
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (typeof(start) !== "number" || typeof(end) !== "number") {
+        return "ERROR"
+    }
+
+    if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+    let variable = start;
+
+    while (variable <= end) {
+        sum += variable;
+        variable++;
+    }
+    return sum;
 };
 
 // Do not edit below this line

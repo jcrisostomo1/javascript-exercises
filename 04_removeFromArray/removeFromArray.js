c3c4a22ec3b232bb27803dccd17bcd3df490a4ac
Josh Crisostomo <joshuacr22@gmail.com>
2022-06-20 21:43:40 -0400

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...removeFromArray) {
+    console.log(removeFromArray)
+    return arr.filter(item => !removeFromArray.includes(item));
 };
+
+let sampleArr = [0,1,2,3,4,5];
+console.log(removeFromArray(sampleArr, 3,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;

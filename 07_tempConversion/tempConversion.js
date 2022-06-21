053192b30e92b40a1349339e45ffa8309a8280a1
Josh Crisostomo <joshuacr22@gmail.com>
2022-06-21 09:27:59 -0400

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+// fahrenheit to celsius
+const ftoc = function(fahrenheit) {
+    let celsius = (fahrenheit -32) * (5/9);
+    return Math.round(celsius * 10) / 10
 };
 
-const ctof = function() {
-
+//celsius to fahrenheit
+const ctof = function(celsius) {
+    let fahrenheit = (celsius * 9/5) + 32;
+    return Math.round(fahrenheit * 10) / 10
 };
+
+console.log(ctof(60));
+console.log(ftoc(32)); 
 
 // Do not edit below this line
 module.exports = {

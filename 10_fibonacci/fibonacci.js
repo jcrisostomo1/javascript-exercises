806e18953c1e0fd3c9ca78397d9bedd4fbb4fa3f
Josh Crisostomo <joshuacr22@gmail.com>
2022-06-24 19:39:33 -0400

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let arr = [0, 1];
+    for (let i = 2; i < num + 1; i++){
+      arr.push(arr[i - 2] + arr[i -1])
+    }
+   return arr[num]
 
 };
 

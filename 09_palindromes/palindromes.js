79e68977d21561e8395f3e6ada5bf5dd72390ce1
Josh Crisostomo <joshuacr22@gmail.com>
2022-06-24 14:27:35 -0400

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const regex = /[^A-Za-z0-9]/g;
+    let trimmedStr = str.toLowerCase().replace(regex, "");
+    console.log(trimmedStr)
+    let copy = trimmedStr;
+    if (trimmedStr === copy.split("").reverse().join("")) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 

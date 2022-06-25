da7c3011ff2f3e701c65436382924f34708a0f3c
Josh Crisostomo <joshuacr22@gmail.com>
2022-06-25 00:42:44 -0400

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = people.map(person => ({
+        name : person.name,
+        age: person.hasOwnProperty('yearOfDeath')? person.yearOfDeath - person.yearOfBirth: new Date().getFullYear()-person.yearOfBirth }));
+    let answer = ages.reduce((acc, curr) => {
+        if (curr.age > acc.age) {
+            acc.age = curr.age;
+            acc.name = curr.name;
+        }
+        return acc;
+    }, {name: "", age: 0});
+    return answer;
 };
 
 // Do not edit below this line
